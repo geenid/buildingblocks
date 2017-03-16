469f4f85657c3163a27cd55968920c56f11cb38e
dsanz <dsanz@plainconcepts.com>
2017-03-16 13:30:01 +0100

    Refactoring integration events: move event bus implementation to building block projects.

---
+++ EventBus/EventBus/Events/EventStateEnum.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Microsoft.eShopOnContainers.BuildingBlocks.EventBus.Events
+{
+    public enum EventStateEnum
+    {
+        NotPublished = 0,
+        Published = 1,
+        PublishedFailed = 2
+    }
+}
---
+++ EventBus/EventBus/Events/IntegrationEvent.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Microsoft.eShopOnContainers.BuildingBlocks.EventBus.Events
+{
+    public class IntegrationEvent
+    {
+        public IntegrationEvent()
+        {
+            Id = Guid.NewGuid();
+        }
+
+        public Guid Id  { get; private set; }
+    }
+}
